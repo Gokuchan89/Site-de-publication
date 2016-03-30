0d26208204049367823a6a5450bd3b9bd38f62f0
Gokuchan89 <albator26@gmail.com>
2016-03-30 19:29:28 +0200

    Added files via upload

--- template/bootstrap/pages/list.php
+++ template/bootstrap/pages/list.php
@@ -492,14 +492,14 @@
 <nav class="navbar navbar-default">
 	<div class="container-fluid">
 		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2" aria-expanded="false">
 				<span class="sr-only">Toggle navigation</span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
 		</div>
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
 			<div class="row">
 				<div class="col-xs-12 col-sm-4 col-md-4">
 					<div class="form-group">
