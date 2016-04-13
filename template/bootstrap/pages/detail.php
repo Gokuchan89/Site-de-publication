cc32860de4140aa2406552ff369b96da9b4c7560
Gokuchan89 <albator26@gmail.com>
2016-04-13 21:19:20 +0200

    Added files via upload

--- template/bootstrap/pages/detail.php
+++ template/bootstrap/pages/detail.php
@@ -1,1 +1,24 @@
-<?php echo $_GET['table']; ?><br/><?php echo $_GET['id']; ?>
+<?php
+	// Protection des variables
+	$_ = array_merge($_GET, $_POST);
+	foreach($_ as $key=>&$val)
+	{
+		Functions::secure($val);
+	}
+	
+	$menu_query = $db->prepare('SELECT `id`, `table`, `type` FROM site_menu WHERE `id` = "'.$table.'"');
+	$menu_query->execute();
+	$menu = $menu_query->fetch();
+	$menu_query->closeCursor();
+	
+	$query = $db->prepare('SELECT * FROM '.$menu['table'].' WHERE `ID` = '.$id);
+	$query->execute();
+	$detail = $query->fetch();
+	$query->closeCursor();
+?>
+<?php echo $menu['table']; ?>
+<br/>
+<?php echo $id; ?>
+<br/>
+<?php echo $detail['TitreVF']; ?>
+
