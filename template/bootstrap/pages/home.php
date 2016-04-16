3130e3c9953546170e1b6864ae59d71590fd5e62
Gokuchan89 <albator26@gmail.com>
2016-04-16 16:00:23 +0200

    Added files via upload

--- template/bootstrap/pages/home.php
+++ template/bootstrap/pages/home.php
@@ -1,12 +1,17 @@
 <?php while($home = $home_query->fetch()) { ?>
+	<?php
+		$lastupdate_query = $db->prepare('SELECT `ID`, `TitreVF`, `EntreeType` FROM '.$home['table'].' ORDER BY ID DESC LIMIT '.$config['lastaddMax']);
+		$lastupdate_query->execute();
+		
+		$total_query = $db->prepare('SELECT COUNT(ID) FROM '.$home['table']);
+		$total_query->execute();
+		$total = $total_query->fetchColumn();
+		$total_query->closeCursor();
+	?>
 	<div class="panel panel-default">
-		<div class="panel-heading"><?php echo 'Les '.$config['lastaddMax'].' derniers ajouts de la catégorie <a href="./?op=list&table='.$home['id'].'">'.$home['name'].'</a>'; ?></div>
+		<div class="panel-heading">Les derniers ajouts de la catégorie <a href="./?op=list&table=<?php echo $home['id']; ?>"><?php echo $home['name']; ?></a><div class="pull-right">(<?php echo $config['lastaddMax']; ?> sur <?php echo $total; ?>)</div></div>
 		<div class="panel-body">
 			<div class="row text-center">
-				<?php
-					$lastupdate_query = $db->prepare('SELECT `ID`, `TitreVF`, `EntreeType` FROM '.$home['table'].' ORDER BY ID DESC LIMIT '.$config['lastaddMax']);
-					$lastupdate_query->execute();
-				?>
 				<?php while($lastupdate = $lastupdate_query->fetch()) { ?>
 					<div class="col-xs-6 col-sm-4 col-md-2">
 						<a href="./?op=detail&table=<?php echo $home['id']; ?>&id=<?php echo $lastupdate['ID']; ?>">
