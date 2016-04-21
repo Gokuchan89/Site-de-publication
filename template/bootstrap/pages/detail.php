fef6590c65c72f7e28cb07a597205bfb9effbb4e
Gokuchan89 <albator26@gmail.com>
2016-04-21 17:13:39 +0200

    Added files via upload

--- template/bootstrap/pages/detail.php
+++ template/bootstrap/pages/detail.php
@@ -32,13 +32,32 @@
 		<!-- DETAILS -->
 		<div class="panel">
 			<li class="list-group-item"><i class="fa fa-pencil"></i> <?php echo $detail['TitreVF']; ?></li>
-			<?php if ($detail['TitreVO'] != '') { ?><li class="list-group-item"><i class="fa fa-pencil"></i> <?php echo $detail['TitreVO']; ?></li><?php } ?>
+			<?php if (!empty($detail['TitreVO'])) { ?><li class="list-group-item"><i class="fa fa-pencil"></i> <?php echo $detail['TitreVO']; ?></li><?php } ?>
 			<li class="list-group-item"><i class="fa fa-tag"></i> <?php echo filter('genre', $detail['Genre'], $menu['id'], $menu['table']); ?></li>
 			<li class="list-group-item">
 				<i class="fa fa-calendar"></i> <?php echo filter('annee', $detail['Annee'], $menu['id'], $menu['table']); ?><br />
-				<?php if ($menu['type'] == 'livre') echo '<i class="fa fa-clock-o"></i> '.$detail['Duree'].' pages<br />'; ?>
-				<?php if ($menu['type'] == 'musique' || $menu['type'] == 'video') echo '<i class="fa fa-clock-o"></i> '.floor($detail['Duree']/60).'h '.($detail['Duree']%60).'min<br />'; ?>
-				<?php if( $menu['type'] == 'video') { ?><i class="fa fa-globe"></i> <?php echo $detail['Pays']; ?><?php } ?>
+				<?php
+					if ($detail['Duree'] != '0')
+					{
+						if ($menu['type'] == 'livre')
+						{
+							echo '<i class="fa fa-book"></i> '.$detail['Duree'].' pages<br />';
+						}
+						if ($menu['type'] == 'musique' || $menu['type'] == 'video')
+						{
+							if ($detail['Duree'] < '60')
+							{
+								$duree = ($detail['Duree']%60).'min';
+							}
+							elseif ($detail['Duree'] > '60')
+							{
+								$duree = floor($detail['Duree']/60).'h '.($detail['Duree']%60).'min';
+							}
+							echo '<i class="fa fa-clock-o"></i> '.$duree.'<br />';
+						}
+					}
+				?>
+				<?php if ($menu['type'] == 'video') { ?><i class="fa fa-globe"></i> <?php echo $detail['Pays']; ?><?php } ?>
 			</li>
 			<?php if ($menu['type'] == 'video') { ?>
 				<li class="list-group-item"><i class="fa fa-thumbs-o-up"></i> <img src="./img/note<?php echo $detail['Note']; ?>.png" alt="note"/></li>
