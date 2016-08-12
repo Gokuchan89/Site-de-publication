22cccebe855edf43324f5c6408241fd538d8763b
Gokuchan89 <albator26@gmail.com>
2016-08-12 18:32:12 +0200

    Add files via upload

--- template/bootstrap/pages/list.php
+++ template/bootstrap/pages/list.php
@@ -431,7 +431,7 @@
 						<?php if ($menu['type'] == 'video') { ?>
 							<div class="row">
 								<div class="col-xs-4 col-sm-4 col-md-3"><strong>Note</strong></div>
-								<div class="col-xs-8 col-sm-8 col-md-9"><span class="text-danger"><img src="img/stars/<?php echo $listing['Note']; ?>.png" /></span></div>
+								<div class="col-xs-8 col-sm-8 col-md-9"><span class="text-danger"><?php if (file_exists('./img/stars/'.$listing['Note'].'.png')) echo '<img src="img/stars/'.$listing['Note'].'.png" />'; else echo $listing['Note']; ?></span></div>
 							</div>
 						<?php } ?>
 					</td>
