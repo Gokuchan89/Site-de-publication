dc28ff1500b9e74fb72bbc09a8a5f0d4be6ad80e
Gokuchan89 <albator26@gmail.com>
2016-09-23 11:39:19 +0200

    suppr

--- themes/bootstrap_v2/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer">
-    <div class="container">
-        <p class="text-muted">
-			<strong>Copyright <i class="fa fa-copyright"></i><?php echo date('Y'); ?></strong>
-			<span class="pull-right"><a href="https://www.filmotech.fr/" target="_blank">Filmotech</a> / <a href="http://getbootstrap.com/" target="_blank">Bootstrap</a></span>
-		</p>
-    </div>
-</footer>
