9bd63e2bd95c69da9661f3bec6555585b60dc750
Gokuchan89 <albator26@gmail.com>
2020-10-28 22:06:23 +0100

    Update footer.php

--- template/bootstrap/includes/footer.php
+++ template/bootstrap/includes/footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <p class="text-muted">
 			<strong>Copyright <i class="fa fa-copyright"></i><?php echo date('Y'); ?></strong>
-			<span class="pull-right"><a href="https://www.filmotech.fr/" target="_blank">Filmotech</a> / <a href="http://getbootstrap.com/" target="_blank">Bootstrap</a></span>
+			<span class="pull-right"><a href="https://www.filmotech.fr/" target="_blank">Filmotech</a> | <a href="http://getbootstrap.com/" target="_blank">Bootstrap</a></span>
 		</p>
     </div>
 </footer>
